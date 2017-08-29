381faf59c88c0461766703a504ef3897ab40bc9d
Mike <583510516@qq.com>
2017-08-29 09:35:16 +0800

    Update PageBean.java

--- PageBean.java
+++ PageBean.java
@@ -16,7 +16,7 @@ public class PageBean {
     }
 
     public int getPageIndex() {
-        return this.pageIndex > this.total ? this.total : this.pageIndex;
+        return this.pageIndex;
     }
 
     private void setPageIndex(int pageIndex) {
